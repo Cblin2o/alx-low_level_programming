d6d607d0a05260677b0cf38cbb2644d1ae5f8ac9
Cblin2o <kolawolerasheed59@gmail.com>
2023-09-12 18:01:51 -0700

    wowo

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**                                                                                                                                                                 
+*main - function prints text                                                                                                                                        *                                                                                                                                                                  
+*Return:return 0                                                                                                                                                    
+*/
+int main(void)
+{
+  _putchar('\n');
+  return (0);
+}
