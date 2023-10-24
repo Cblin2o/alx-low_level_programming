dbda50356652196a4dc4b6fa3b3f297d17d61d0a
Cblin2o <kolawolerasheed59@gmail.com>
2023-10-24 14:56:26 -0700

    initial

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,18 +9,25 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint i;
+	listint_t *new_node;
+	listint_t *cursor = *head;
 
-	if (i != NULL)
-		i-> = n;
-		i->next = NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
 	else
 		return (NULL);
-	if (head != NULL)
-		while (head->next != NULL)
-			head = head->next;
-		head->next = i;
+	if (cursor != NULL)
+	{
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+
+		cursor->next = new_node;
+	}
 	else
-		head = i;
-	return(i);
+		*head = new_node;
+	return (new_node);
 }
