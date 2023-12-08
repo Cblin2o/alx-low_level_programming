e1a539bd5884523466a2981789d06d76958429e2
Cblin2o <kolawolerasheed59@gmail.com>
2023-12-07 16:51:09 -0800

    w

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - print the length of nodes in a list
+ * @h: pointer to the head of a list
+ * Return : length of a list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int x = 0;
+
+	if (h == NULL)
+		return (x);
+	while (h != NULL)
+	{
+		h = h->next;
+		x++;
+	}
+
+	return (x);
+}
